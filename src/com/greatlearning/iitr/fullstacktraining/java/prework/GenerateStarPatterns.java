edd3310768a57d24c26eb946d24d2b8eead6b143
satyasreechatterjee <satyasreechatterjee@192.168.0.100>
2021-08-01 09:13:23 +0530

    Class Files checked in

---
+++ src/com/greatlearning/iitr/fullstacktraining/java/prework/GenerateStarPatterns.java
@@ -0,0 +1,10 @@
+package com.greatlearning.iitr.fullstacktraining.java.prework;
+
+public class GenerateStarPatterns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
